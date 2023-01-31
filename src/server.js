19aa227ccc8c9542db953595840f3e74ae631c78
@brillianita <brillianitaaa@gmail.com>
2023-02-01 00:25:33 +0800

    add authentications & authorization

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const dataRoutes = require('./routes/dataRoutes');
 const kontraktorRoutes = require('./routes/kontraktorRoutes');
 const adminRoutes = require('./routes/adminRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 require('dotenv').config();
 
@@ -27,5 +28,6 @@ app.get('/', (req, res) => {
 app.use(dataRoutes);
 app.use(kontraktorRoutes);
 app.use(adminRoutes);
+app.use(authRoutes);
 
 app.listen(PORT, () => console.log(`API is listening on port ${PORT}`));
