26ef63cac348568ba1ed4c68cb6a32b55f7f2b3b
Nurdita Fahira <dhitafahira22@gmail.com>
2023-02-01 14:46:41 +0800

    statistik monPr

--- src/routes/dataRoutes.js
+++ src/routes/dataRoutes.js
@@ -6,5 +6,6 @@ const router = Router();
 router.get('/data', dataHandler.getData);
 router.get('/statdata', dataHandler.getStatistikbyDataStatus);
 router.get('/statPlanActual/:id_datum', dataHandler.getStatistikPlanVsActual);
+router.get('/statMonPr', dataHandler.getStatistikMonPr);
 
 module.exports = router;
