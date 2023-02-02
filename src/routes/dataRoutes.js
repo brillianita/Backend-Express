b3896006a356646587ccd60b677abbc90737c382
Nurdita Fahira <dhitafahira22@gmail.com>
2023-02-02 12:28:18 +0800

    add handler pic pr

--- src/routes/dataRoutes.js
+++ src/routes/dataRoutes.js
@@ -7,5 +7,6 @@ router.get('/data', dataHandler.getData);
 router.get('/statdata', dataHandler.getStatistikbyDataStatus);
 router.get('/statPlanActual/:id_datum', dataHandler.getStatistikPlanVsActual);
 router.get('/statMonPr', dataHandler.getStatistikMonPr);
+router.get('/statPicPr', dataHandler.getStatistikPrKonstruksi);
 
 module.exports = router;
