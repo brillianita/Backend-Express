01f632270d97b59f564ade0f748b8ed82eb86f2d
Nurdita Fahira <dhitafahira22@gmail.com>
2023-02-04 23:17:35 +0800

    adding delete, edit, and get detail data handler

--- src/handler/dataHandler.js
+++ src/handler/dataHandler.js
@@ -1,21 +1,13 @@
 const pool = require('../config/db');
 const ClientError = require('../exceptions/clientError');
 const InvariantError = require('../exceptions/invariantError');
+const NotFoundError = require('../exceptions/notFoundError');
 
 const addDatum = async (req, res) => {
   try {
     const {
-      nmJenis,
-      tahun,
-      noProyek,
-      namaProyek,
-      namaRekanan,
-      tglMulai,
-      tglAkhir,
-      nilai,
-      nmKota,
-      nmLokasi,
-      keterangan,
+      nmJenis, tahun, noProyek, namaProyek, namaRekanan,
+      tglMulai, tglAkhir, nilai, nmKota, nmLokasi, keterangan,
     } = req.body;
 
     const queryInsert = {
@@ -60,7 +52,6 @@ const getData = async (req, res) => {
     text: 'SELECT * FROM data',
   };
   const data = await pool.query(queryGet);
-  // console.log(data.rows);
 
   return res.status(200).send({
     status: 'success',
@@ -68,6 +59,132 @@ const getData = async (req, res) => {
   });
 };
 
+const getDatum = async (req, res) => {
+  try {
+    const { idDatum } = req.params;
+
+    if (Number.isNaN(Number(idDatum))) {
+      throw new InvariantError('Gagal mengambil data. Mohon isi idDatum proyek dengan benar');
+    }
+
+    const queryGet = {
+      text: 'SELECT * FROM data WHERE id_datum = $1',
+      values: [idDatum],
+    };
+    const poolDatum = await pool.query(queryGet);
+
+    if (!(poolDatum.rows[0])) {
+      throw new NotFoundError(`Data dengan id: ${idDatum} tidak ditemukan`);
+    }
+
+    return res.status(200).send({
+      status: 'success',
+      data: poolDatum.rows[0],
+    });
+  } catch (e) {
+    console.error(e);
+
+    if (e instanceof ClientError) {
+      return res.status(400).send({
+        status: 'fail',
+        message: e.message,
+      });
+    }
+    return res.status(500).send({
+      status: 'error',
+      message: 'Gagal menghapus data',
+    });
+  }
+};
+
+const editDatum = async (req, res) => {
+  try {
+    const { idDatum } = req.params;
+
+    if (!idDatum || Number.isNaN(Number(idDatum))) {
+      throw new InvariantError('Gagal mengedit data. Mohon isi idDatum proyek dengan benar');
+    }
+
+    const {
+      nmJenis, tahun, noProyek, namaProyek, namaRekanan, tglMulai, tglAkhir, nilai,
+      nmKota, nmLokasi, keterangan, tglSelesai, tglBast1, batasRetensi,
+    } = req.body;
+
+    const queryUpdate = {
+      text: 'UPDATE data SET nm_jenis = $1, tahun = $2, no_proyek = $3, nm_proyek = $4, nm_rekanan = $5, tgl_mulai = $6, tgl_akhir = $7, nilai = $8, nm_kota = $9, nm_lokasi = $10, keterangan = $11, tgl_selesai = $12, tgl_bast1 = $13, batas_retensi = $14 WHERE id_datum = $15 RETURNING *;',
+      values: [
+        nmJenis, tahun, noProyek, namaProyek, namaRekanan, tglMulai,
+        tglAkhir, nilai, nmKota, nmLokasi, keterangan, tglSelesai, tglBast1, batasRetensi, idDatum,
+      ],
+    };
+    let poolRes;
+    try {
+      poolRes = await pool.query(queryUpdate);
+    } catch (e) {
+      throw new InvariantError(e);
+    }
+    if (!poolRes.rows[0]) {
+      throw new NotFoundError(`Tidak dapat menemukan data ${idDatum}`);
+    }
+    return res.status(201).send({
+      status: 'success',
+      message: 'Berhasil mengupdate data',
+      data: poolRes.rows[0],
+    });
+  } catch (e) {
+    console.error(e);
+
+    if (e instanceof ClientError) {
+      return res.status(400).send({
+        status: 'fail',
+        message: e.message,
+      });
+    }
+    return res.status(500).send({
+      status: 'error',
+      message: 'Gagal menghapus data',
+    });
+  }
+};
+
+const deleteDatum = async (req, res) => {
+  try {
+    const { idDatum } = req.params;
+
+    if (!idDatum || Number.isNaN(Number(idDatum))) {
+      throw new InvariantError('Gagal menghapus data. Mohon isi idDatum proyek dengan benar');
+    }
+
+    const queryDel = {
+      text: 'DELETE FROM data WHERE id_datum = $1 RETURNING nm_proyek',
+      values: [idDatum],
+    };
+    const poolDel = await pool.query(queryDel);
+
+    if (!(poolDel.rows[0])) {
+      throw new NotFoundError(`Data dengan id: ${idDatum} tidak ditemukan`);
+    }
+
+    return res.status(200).send({
+      status: 'success',
+      message: `Data proyek ${poolDel.rows[0].nm_proyek} berhasil dihapus`,
+    });
+  } catch (e) {
+    console.error(e);
+
+    if (e instanceof ClientError) {
+      return res.status(400).send({
+        status: 'fail',
+        message: e.message,
+      });
+    }
+    return res.status(500).send({
+      status: 'error',
+      message: 'Gagal menghapus data',
+    });
+  }
+};
+
 module.exports = {
-  getData, addDatum,
+  getData, addDatum, deleteDatum, getDatum, editDatum,
 };
--- src/routes/dataRoutes.js
+++ src/routes/dataRoutes.js
@@ -4,6 +4,10 @@ const dataHandler = require('../handler/dataHandler');
 const router = Router();
 
 router.get('/data', dataHandler.getData);
+router.get('/data/:idDatum', dataHandler.getDatum);
 router.post('/data', dataHandler.addDatum);
+router.put('/data/:idDatum', dataHandler.editDatum);
+router.delete('/data/:idDatum', dataHandler.deleteDatum);
+router.delete('/data', dataHandler.deleteDatum);
 
 module.exports = router;
