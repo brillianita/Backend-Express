b0cb253d5d71844f9918d96b2c6f96ed4df04b95
@brillianita <brillianitaaa@gmail.com>
2023-02-05 23:34:27 +0800

    add update, detail laporan for kontraktor

--- src/routes/laporanRoutes.js
+++ src/routes/laporanRoutes.js
@@ -5,6 +5,8 @@ const uploadFile = require('../middleware/uploadFile');
 const router = Router();
 router.post('/laporan/tambah', uploadFile.upload.single('file'), laporanHandler.createLaporan);
 router.get('/laporan/:nomorKontrak', laporanHandler.getLaporan);
+router.get('/detailLaporan/:id', laporanHandler.getLaporanDetail);
 router.get('/file/:name', laporanHandler.download);
+router.put('/laporan/edit/:id', uploadFile.upload.single('file'), laporanHandler.updateLaporan);
 
 module.exports = router;
