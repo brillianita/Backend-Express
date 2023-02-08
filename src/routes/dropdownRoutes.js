65af4ff55537d8a45b46c54853f2d60d779475ee
Nurdita Fahira <dhitafahira22@gmail.com>
2023-02-08 19:55:11 +0800

    search proyek

--- src/routes/dropdownRoutes.js
+++ src/routes/dropdownRoutes.js
@@ -5,5 +5,6 @@ const router = Router();
 
 router.get('/dropdown/proyek', dropdownHandler.dropdownProyek);
 router.get('/dropdown/kontraktor', dropdownHandler.dropdownKontraktor);
+router.get('/search', dropdownHandler.searchProyek);
 
 module.exports = router;
