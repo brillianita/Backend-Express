d7aa1d011ce9e8b2e8059a97ca9248ac386c7316
@brillianita <brillianitaaa@gmail.com>
2023-02-14 02:42:31 +0800

    FIX the errorhandling for kontraktorHandler

--- src/handler/kontraktorHandler.js
+++ src/handler/kontraktorHandler.js
@@ -1,23 +1,40 @@
 const bcrypt = require('bcryptjs');
 const pool = require('../config/db');
+const ClientError = require('../exceptions/clientError');
+const InvariantError = require('../exceptions/invariantError');
+const NotFoundError = require('../exceptions/notFoundError');
+const AuthenticationError = require('../exceptions/authError');
+
+const arraySeperator = (arrData) => {
+  let arrDataStr = '';
+  for (let i = 0; i < arrData.length; i += 1) {
+    if (i === arrData.length - 1) {
+      arrDataStr += `'${(arrData[i])}'`;
+    } else {
+      arrDataStr += `'${(arrData[i])}',`;
+    }
+  }
+  return arrDataStr;
+};
 
 const getAllKontraktor = async (req, res) => {
-  const { pageSize, currentPage, search } = req.query;
-
   try {
+    const { pageSize, currentPage, search } = req.query;
     let qFilter;
     if (!search) {
-      qFilter = "SELECT u.username, u.id AS id_user , d.no_proyek, d.nm_rekanan, d.klasifikasi, d.nm_proyek, d.nm_lokasi FROM users AS u INNER JOIN kontraktor_conn AS k ON u.id = k.id_user INNER JOIN data AS d ON d.id_datum = k.id_datum WHERE role = 'kontraktor' ORDER BY LOWER(u.username) ASC";
+      qFilter = "SELECT u.id AS id_user, u.username , d.no_proyek, d.nm_proyek, d.nm_rekanan, d.klasifikasi, d.nm_lokasi FROM users AS u INNER JOIN kontraktor_conn AS k ON u.id = k.id_user INNER JOIN data AS d ON d.id_datum = k.id_datum WHERE role = 'kontraktor' ORDER BY LOWER(u.no_proyek) ASC";
     } else {
-      qFilter = `SELECT u.username, u.id AS id_user , d.no_proyek, d.nm_rekanan, d.klasifikasi, d.nm_proyek, d.nm_lokasi FROM users AS u INNER JOIN kontraktor_conn AS k ON u.id = k.id_user INNER JOIN data AS d ON d.id_datum = k.id_datum WHERE LOWER(u.username) LIKE LOWER('%${search}%') OR LOWER(d.no_proyek) LIKE LOWER('%${search}%') AND role = 'kontraktor' ORDER BY LOWER(u.username) ASC`;
+      qFilter = `SELECT u.id AS id_user, u.username , d.no_proyek, d.nm_proyek, d.nm_rekanan, d.klasifikasi, d.nm_lokasi FROM users AS u INNER JOIN kontraktor_conn AS k ON u.id = k.id_user INNER JOIN data AS d ON d.id_datum = k.id_datum WHERE LOWER(d.no_proyek) LIKE LOWER('%${search}%') OR LOWER(d.nm_proyek) LIKE LOWER('%${search}%') OR LOWER(d.nm_rekanan) LIKE LOWER('%${search}%') OR LOWER(d.klasifikasi)OR LOWER(d.nm_lokasi) LIKE LOWER('%${search}%') LIKE LOWER('%${search}%') AND role = 'kontraktor' ORDER BY LOWER(u.no_proyek) ASC`;
     }
     let result = await pool.query(qFilter);
 
     if (pageSize && currentPage) {
-      const totalRows = await pool.query(`SELECT COUNT (id) FROM (${qFilter})sub`);
+      const totalRows = await pool.query(`SELECT COUNT (id_user) FROM (${qFilter})sub`);
       const totalPages = Math.ceil(totalRows.rows[0].count / pageSize);
       const offset = (currentPage - 1) * pageSize;
-      result = await pool.query(`SELECT * FROM (${qFilter})sub ORDER BY LOWER(username) ASC LIMIT ${pageSize} OFFSET ${offset};`);
+
+      result = await pool.query(`SELECT * FROM (${qFilter})sub ORDER BY LOWER(no_proyek) ASC LIMIT ${pageSize} OFFSET ${offset};`);
+
       return res.status(200).send({
         status: 'success',
         data: result.rows,
@@ -29,7 +46,7 @@ const getAllKontraktor = async (req, res) => {
         },
       });
     }
-    result = await pool.query(`SELECT * FROM (${qFilter})sub ORDER BY LOWER(username) ASC;`);
+    result = await pool.query(`SELECT * FROM (${qFilter})sub ORDER BY LOWER(no_proyek) ASC;`);
 
     return res.status(200).send({
       status: 'success',
@@ -38,7 +55,6 @@ const getAllKontraktor = async (req, res) => {
   } catch (e) {
     return res.status(500).send({
       status: 'error',
-      // message: "Sorry there was a failure on our server."
       message: e.message,
     });
   }
@@ -48,15 +64,15 @@ const getKontraktorById = async (req, res) => {
   try {
     const { id } = req.params;
     const query = {
-      text: "SELECT u.username, u.id AS id_user , d.no_proyek, d.nm_rekanan, d.klasifikasi, d.nm_proyek, d.nm_lokasi FROM users AS u INNER JOIN kontraktor_conn AS k ON u.id = k.id_user INNER JOIN data AS d ON d.id_datum = k.id_datum WHERE role = 'kontraktor' AND u.id = $1",
+      text: "SELECT u.id AS id_user, u.username , d.no_proyek, d.nm_proyek, d.nm_rekanan, d.klasifikasi, d.nm_lokasi FROM users AS u INNER JOIN kontraktor_conn AS k ON u.id = k.id_user INNER JOIN data AS d ON d.id_datum = k.id_datum WHERE role = 'kontraktor' AND u.id = $1",
       values: [id],
     };
     const result = await pool.query(query);
 
     // Check if data is null
-    // if (!result.rows.length) {
-    //     throw new NotFoundError(`User Not Found!`);
-    // }
+    if (!result.rows.length) {
+      throw new NotFoundError(`Gagal menampilkan data kontraktor. Kontraktor dengan ${id} tidak ditemukan`);
+    }
 
     return res.status(200).send({
       status: 'success',
@@ -64,13 +80,12 @@ const getKontraktorById = async (req, res) => {
 
     });
   } catch (e) {
-    // console.log(e);
-    // if (e instanceof ClientError) {
-    //     res.status(e.statusCode).send({
-    //         status: 'fail',
-    //         message: e.message,
-    //     })
-    // }
+    if (e instanceof ClientError) {
+      res.status(e.statusCode).send({
+        status: 'fail',
+        message: e.message,
+      });
+    }
     return res.status(500).send({
       status: 'error',
       message: e.message,
@@ -78,44 +93,52 @@ const getKontraktorById = async (req, res) => {
   }
 };
 
-// Create user
 const createKontraktor = async (req, res) => {
-  const {
-    noProyek,
-    username,
-    password,
-    confirmPassword,
-  } = req.body;
-
-  const hashPassword = bcrypt.hashSync(password, 8);
-  if (password !== confirmPassword) {
-    return res.status(400).send({
-      status: 'fail',
-      message: 'Password and Confirm Password does not match',
-    });
-  }
-
   try {
+    const {
+      noProyek,
+      username,
+      password,
+      confirmPassword,
+    } = req.body;
+
+    const noProyekStr = arraySeperator(noProyek);
+    const qGetData = {
+      text: `SELECT id_datum, no_proyek FROM data WHERE no_proyek in (${noProyekStr}) ORDER BY id_datum ASC`,
+    };
+    const resData = await pool.query(qGetData);
+
+    const idDatum = [];
+    for (let i = 0; i < noProyek.length; i += 1) {
+      if (!resData.rows[i]) {
+        throw new InvariantError(`noProyek ${noProyek[i]} tidak valid`);
+      }
+      idDatum.push(resData.rows[i].id_datum);
+    }
+
+    const arrIdStr = arraySeperator(idDatum);
+    const qGetKon = {
+      text: `SELECT k.id_datum, k.id_user, d.no_proyek FROM kontraktor_conn AS k INNER JOIN data AS d ON k.id_datum = d.id_datum WHERE k.id_datum in (${arrIdStr}) ORDER BY id_datum ASC`,
+    };
+    const resKon = await pool.query(qGetKon);
+    for (let i = 0; i < resKon.rows.length; i += 1) {
+      if (resKon.rows[i]) {
+        throw new InvariantError(`Gagal membuat akun kontraktor. noProyek ${resKon.rows[i].no_proyek} telah digunakan di akun kontraktor lain`);
+      }
+    }
+
+    const hashPassword = bcrypt.hashSync(password, 8);
+    if (password !== confirmPassword) {
+      throw new InvariantError('Gagal membuat akun kontraktor. Password dan konfirmasi password tidak sama');
+    }
+
     const qAddUser = {
       text: 'INSERT INTO users (id, username, password, role) VALUES (DEFAULT, $1, $2, $3) RETURNING id;',
       values: [username, hashPassword, 'kontraktor'],
     };
     const resUser = await pool.query(qAddUser);
 
-    let noProyekStr = '';
-    for (let i = 0; i < noProyek.length; i += 1) {
-      if (i === noProyek.length - 1) {
-        noProyekStr += `'${(noProyek[i])}'`;
-      } else {
-        noProyekStr += `'${(noProyek[i])}',`;
-      }
-    }
-    const qGetData = {
-      text: `SELECT id_datum, no_proyek FROM data WHERE no_proyek in (${noProyekStr})`,
-      // values: [noProyekStr],
-    };
-    const resData = await pool.query(qGetData);
-
+    const promises = [];
     let qNoKontrak;
     for (let i = 0; i < resData.rows.length; i += 1) {
       qNoKontrak = {
@@ -125,15 +148,22 @@ const createKontraktor = async (req, res) => {
           resUser.rows[0].id,
         ],
       };
-      pool.query(qNoKontrak);
-    }
 
-    // await pool.query(qUser);
+      promises.push(pool.query(qNoKontrak));
+    }
+    await Promise.all(promises);
+
     return res.status(201).send({
       status: 'success',
       message: 'Register Successfull!',
     });
   } catch (e) {
+    if (e instanceof ClientError) {
+      return res.status(400).send({
+        status: 'fail',
+        message: e.message,
+      });
+    }
     return res.status(500).send({
       status: 'error',
       message: e.message,
@@ -142,52 +172,64 @@ const createKontraktor = async (req, res) => {
 };
 
 const updateKontraktor = async (req, res) => {
-  const { id } = req.params;
-  const {
-    noProyek,
-    username,
-    oldPass,
-    newPass,
-    confirmNewPass,
-  } = req.body;
-
   try {
+    const { id } = req.params;
+    const {
+      noProyek,
+      username,
+      oldPass,
+      newPass,
+      confirmNewPass,
+    } = req.body;
+
     const qGetUser = {
       text: 'SELECT id, password, role FROM users WHERE id=$1',
       values: [id],
     };
     const resGetUser = await pool.query(qGetUser);
-    if (resGetUser.rows[0].role !== 'kontraktor') {
-      return res.status(401).send({
-        status: 'fail',
-        message: 'invalid request',
-      });
+    const dataGetUser = resGetUser.rows;
+    if (!dataGetUser.length || dataGetUser[0].role !== 'kontraktor') {
+      throw new InvariantError('Gagal mengubah data kontraktor. Akun ini bukan role kontraktor atau akun tidak ditemukan');
     }
+
+    const noProyekStr = arraySeperator(noProyek);
+    const qGetData = {
+      text: `SELECT id_datum, no_proyek FROM data WHERE no_proyek in (${noProyekStr}) ORDER BY id_datum ASC`,
+    };
+    const resData = await pool.query(qGetData);
+    const idDatum = [];
+    for (let i = 0; i < noProyek.length; i += 1) {
+      if (!resData.rows[i]) {
+        throw new InvariantError(`noProyek ${noProyek[i]} tidak valid`);
+      }
+      idDatum.push(resData.rows[i].id_datum);
+    }
+
+    const arrIdStr = arraySeperator(idDatum);
+    const qGetKon = {
+      text: `SELECT k.id_datum, k.id_user, d.no_proyek FROM kontraktor_conn AS k INNER JOIN data AS d ON k.id_datum = d.id_datum WHERE k.id_datum in (${arrIdStr}) ORDER BY id_datum ASC`,
+    };
+    const resKon = await pool.query(qGetKon);
+    for (let i = 0; i < resKon.rows.length; i += 1) {
+      // eslint-disable-next-line radix
+      if (resKon.rows[i].id_user !== parseInt(id) && resKon.rows[i]) {
+        throw new InvariantError(`Gagal mengubah data kontraktor. noProyek ${resKon.rows[i].no_proyek} telah digunakan di akun kontraktor lain`);
+      }
+    }
+
     const qUpUsername = {
       text: 'UPDATE users SET username = $1  WHERE id = $2;',
       values: [username, id],
     };
     await pool.query(qUpUsername);
 
-    let noProyekStr = '';
-    for (let i = 0; i < noProyek.length; i += 1) {
-      if (i === noProyek.length - 1) {
-        noProyekStr += `'${(noProyek[i])}'`;
-      } else {
-        noProyekStr += `'${(noProyek[i])}',`;
-      }
-    }
-    const qGetData = {
-      text: `SELECT id_datum, no_proyek FROM data WHERE no_proyek in (${noProyekStr})`,
-      // values: [noProyekStr],
-    };
-    const resData = await pool.query(qGetData);
-
     const qDelKontrak = {
       text: 'DELETE FROM kontraktor_conn WHERE id_user = $1',
       values: [id],
     };
     await pool.query(qDelKontrak);
+
+    const promises = [];
     let qNoKontrak;
     for (let i = 0; i < resData.rows.length; i += 1) {
       qNoKontrak = {
@@ -197,40 +239,42 @@ const updateKontraktor = async (req, res) => {
           id,
         ],
       };
-      pool.query(qNoKontrak);
+      promises.push(pool.query(qNoKontrak));
     }
+    await Promise.all(promises);
 
     if (oldPass && newPass && confirmNewPass) {
       const passwordIsValid = bcrypt.compareSync(
         oldPass,
-        resGetUser.rows[0].password,
+        dataGetUser[0].password,
       );
       // If the password is not valid, send the error message
       if (!passwordIsValid) {
-        return res.status(401).send({
-          status: 'fail',
-          message: 'Incorrect old password!',
-        });
-        // throw new AuthenticationError('Invalid Password!');
+        throw new AuthenticationError('Gagal mengubah password. Password lama salah');
       }
-      const hashPassword = bcrypt.hashSync(newPass, 8);
       if (newPass !== confirmNewPass) {
-        return res.status(400).send({
-          status: 'fail',
-          message: 'Password and confirm password does not match',
-        });
+        throw new InvariantError('Gagal mengubah password. Password baru dan Konfirmasi Password tidak sama');
       }
+
+      const hashPassword = bcrypt.hashSync(newPass, 8);
       const qUpdatePass = {
         text: 'UPDATE users SET password = $1  WHERE id = $2;',
         values: [hashPassword, id],
       };
       await pool.query(qUpdatePass);
     }
+
     return res.status(201).send({
       status: 'success',
-      message: 'Staff has been updated',
+      message: 'Kontraktor has been updated',
     });
   } catch (e) {
+    if (e instanceof ClientError) {
+      res.status(e.statusCode).send({
+        status: 'fail',
+        message: e.message,
+      });
+    }
     return res.status(500).send({
       status: 'error',
       message: e.message,
@@ -239,18 +283,15 @@ const updateKontraktor = async (req, res) => {
 };
 
 const deleteKontraktor = async (req, res) => {
-  const { id } = req.params;
   try {
+    const { id } = req.params;
     const queryKontraktor = {
       text: 'SELECT * FROM kontraktor_conn WHERE id_user = $1',
       values: [id],
     };
     const resKontraktor = await pool.query(queryKontraktor);
     if (!resKontraktor.rows[0]) {
-      return res.status(404).send({
-        status: 'fail',
-        message: 'User not found',
-      });
+      throw new NotFoundError(`Gagal menghapus data kontraktor. Kontraktor dengan ${id} tidak ditemukan`);
     }
     const queryDelete = {
       text: 'DELETE FROM users WHERE id=$1',
@@ -262,6 +303,12 @@ const deleteKontraktor = async (req, res) => {
       message: 'User has been removed',
     });
   } catch (e) {
+    if (e instanceof ClientError) {
+      res.status(e.statusCode).send({
+        status: 'fail',
+        message: e.message,
+      });
+    }
     return res.status(500).send({
       status: 'error',
       message: e.message,
