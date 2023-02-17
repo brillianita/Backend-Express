b7b543079a5a73dc01b7c03508b6d7c0d91b5df3
Nurdita Fahira <dhitafahira22@gmail.com>
2023-02-18 07:38:15 +0800

    get dan upate detail laphar

--- src/handler/laporanHandler.js
+++ src/handler/laporanHandler.js
@@ -572,6 +572,250 @@ const createLapHarian = async (req, res) => {
   }
 };
 
+const getDetailLapHarian = async (req, res) => {
+  try {
+    // id dari tabel laporan
+    const { id } = req.params;
+
+    const queryGetInfoLH = {
+      text: 'SELECT d.id_datum, d.nm_proyek as pekerjaan, d.nm_rekanan as vendor, DATE(lh.tgl) as tanggal FROM data as d INNER JOIN laporan as l ON l.id_datum = d.id_datum INNER JOIN lap_harian as lh ON lh.id_laporan = l.id WHERE l.id = $1;',
+      values: [id],
+    };
+    const poolInfoLH = await pool.query(queryGetInfoLH);
+    const infoLH = poolInfoLH.rows[0];
+
+    const queryGetDescLH = {
+      text: 'SELECT lh.id as id_lapharian, lh.aktivitas as aktivitas, lh.rencana as rencana, lh.note as note FROM lap_harian as lh INNER JOIN laporan as l ON l.id = lh.id_laporan WHERE l.id = $1 ORDER BY lh.id',
+      values: [id],
+    };
+    const poolDescLH = await pool.query(queryGetDescLH);
+    const descLH = poolDescLH.rows[0];
+
+    const queryGetTenaKer = {
+      text: "SELECT tk.id, tk.jabatan, tk.jmlh FROM tenaga_kerja as tk INNER JOIN lap_harian as lh ON tk.id_lap_harian = lh.id INNER JOIN laporan as l ON l.id = lh.id_laporan WHERE l.id = $1 AND tk.status_hari = 'hari ini' ORDER BY tk.id",
+      values: [id],
+    };
+    const queryGetTenaKerB = {
+      text: "SELECT tk.id, tk.jabatan, tk.jmlh FROM tenaga_kerja as tk INNER JOIN lap_harian as lh ON tk.id_lap_harian = lh.id INNER JOIN laporan as l ON l.id = lh.id_laporan WHERE l.id = $1 AND tk.status_hari = 'besok' ORDER BY tk.id",
+      values: [id],
+    };
+    const poolTenaKer = await pool.query(queryGetTenaKer);
+    const poolTenaKerB = await pool.query(queryGetTenaKerB);
+
+    const queryGetAlatKerja = {
+      text: 'SELECT ak.id, ak.alat, ak.qty FROM alat_kerja as ak INNER JOIN lap_harian as lh ON ak.id_lap_harian = lh.id INNER JOIN laporan as l ON l.id = lh.id_laporan WHERE l.id = $1 ORDER BY ak.id',
+      values: [id],
+    };
+    const poolAlatKerja = await pool.query(queryGetAlatKerja);
+
+    const queryGetNote = {
+      text: 'SELECT n.id, n.masalah, n.solusi FROM note as n INNER JOIN lap_harian as lh ON n.id_lap_harian = lh.id INNER JOIN laporan as l ON l.id = lh.id_laporan WHERE l.id = $1 ORDER BY n.id',
+      values: [id],
+    };
+    const poolNote = await pool.query(queryGetNote);
+
+    const queryGetCuaca = {
+      text: 'SELECT c.id, c.baik, c.mendung, c.hujan_tinggi, c.hujan_rendah FROM kond_cuaca as c INNER JOIN lap_harian as lh ON c.id_lap_harian = lh.id INNER JOIN laporan as l ON l.id = lh.id_laporan WHERE l.id = $1 ORDER BY c.id',
+      values: [id],
+    };
+    const poolCuaca = await pool.query(queryGetCuaca);
+
+    // const coba = {
+    //   text: 'SELECT'
+    // }
+
+    descLH.tkToday = poolTenaKer.rows;
+    descLH.tkTomorrow = poolTenaKerB.rows;
+    descLH.alatKerja = poolAlatKerja.rows;
+    descLH.note = poolNote.rows;
+    descLH.cuaca = poolCuaca.rows;
+
+    return res.status(200).send({
+      status: 'success',
+      data: {
+        infoLH,
+        descLH,
+      },
+    });
+  } catch (e) {
+    console.error(e);
+
+    if (e instanceof ClientError) {
+      return res.status(400).send({
+        status: 'fail',
+        message: e.message,
+      });
+    }
+    return res.status(500).send({
+      status: 'error',
+      message: 'Gagal mengambil data',
+    });
+  }
+};
+
+const editDetailLapHarian = async (req, res) => {
+  try {
+    // id dari tabel laporan
+    const { id } = req.params;
+    const {
+      noProyek, idUser, tgl, aktivitas, rencana, note, jabatanhrini, jmlhhrini,
+      jabatanbsk, jmlhbsk, baik, mendung, hujanTinggi, hujanRendah, alat, qty, masalah,
+      solusi, idNote, idTkhrini, idTkbsk, idAlatKerja, idCuaca,
+    } = req.body;
+
+    // Validate the body request
+    // eslint-disable-next-line max-len
+    if (!aktivitas || !rencana || !jabatanhrini || !jmlhhrini || !alat || !qty || !jabatanbsk || !jmlhbsk) {
+      throw new InvariantError('Aktivitas or Rencana or Jabatan or jumlah or Alat or qty wajib diisi!');
+    }
+
+    if (typeof (aktivitas) !== 'object' || typeof (rencana) !== 'object' || typeof (baik) !== 'object' || typeof (mendung) !== 'object' || typeof (hujanTinggi) !== 'object' || typeof (hujanRendah) !== 'object' || typeof (jabatanhrini) !== 'object' || typeof (jmlhhrini) !== 'object' || typeof (jabatanbsk) !== 'object' || typeof (jmlhbsk) !== 'object' || typeof (alat) !== 'object' || typeof (qty) !== 'object' || typeof (masalah) !== 'object' || typeof (solusi) !== 'object') {
+      throw new InvariantError('Pastikan semua tipe data tiap field sudah benar');
+    }
+
+    // eslint-disable-next-line max-len
+    if (jabatanhrini.length !== jmlhhrini.length || jabatanbsk.length !== jmlhbsk.length || alat.length !== qty.length || masalah.length !== solusi.length) {
+      throw new InvariantError('Pastikan panjang field pada array sudah benar');
+    }
+
+    const aktivitasStr = `[${aktivitas}]`;
+    const rencanaStr = `[${rencana}]`;
+    const noteStr = `[${note}]`;
+    const baikStr = `[${baik}]`;
+    const mendungStr = `[${mendung}]`;
+    const hujanTinggiStr = `[${hujanTinggi}]`;
+    const hujanRendahStr = `[${hujanRendah}]`;
+
+    const qIdData = {
+      text: 'SELECT k.id_datum, k.id_user, d.no_proyek FROM kontraktor_conn AS k INNER JOIN data AS d ON k.id_datum = d.id_datum WHERE d.no_proyek = $1',
+      values: [noProyek],
+    };
+    const resQId = await pool.query(qIdData);
+    if (!resQId.rows.length) {
+      throw new NotFoundError(`noProyek ${noProyek} tidak ditemukan`);
+    }
+
+    const qIdKon = {
+      text: 'SELECT id_user FROM kontraktor_conn WHERE id_user = $1',
+      values: [idUser],
+    };
+    const resQIdKon = await pool.query(qIdKon);
+    if (!resQIdKon.rows.length) {
+      throw new NotFoundError('Pengguna tidak ditemukan atau tidak memiliki role kontraktor');
+    }
+
+    const currDate = new Date(new Date().setHours(0, 0, 0, 0)).getTime();
+    const tglLap = Date.parse(tgl);
+    const status = tglLap < currDate ? 'Tepat Waktu' : 'Terlambat';
+
+    // const createdAt = new Date(new Date().setHours(0, 0, 0, 0));
+    // const qLap = {
+    //   textq: 'INSERT INTO laporan (id, jenis_laporan, urutan_lap, created_at, catatan, status, id_datum, id_user) VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7) RETURNING *;',
+    //   text: 'UPDATE laporan SET (urutan_lap, '
+    //   values: ['Laporan Harian', urutanLap, createdAt, null, 'Ditinjau', resQId.rows[0].id_datum, idUser],
+    // };
+    // const rLap = await pool.query(qLap);
+
+    const qLapHar = {
+      text: 'UPDATE lap_harian SET ( aktivitas, rencana, status, tgl, note) VALUES (DEFAULT, $1, $2, $3, $4, $5, $6) WHERE id_laporan = $1 RETURNING *;',
+      values: [aktivitasStr, rencanaStr, status, tgl, noteStr],
+    };
+    const rLapHar = await pool.query(qLapHar);
+
+    const ptenKerjaHrIni = [];
+    let qTenKerjaHrIni;
+    for (let i = 0; i < jabatanhrini.length; i += 1) {
+      qTenKerjaHrIni = {
+        // text: 'INSERT INTO tenaga_kerja (id, jabatan, jmlh, status_hari, id_lap_harian) VALUES (DEFAULT, $1, $2, $3, $4)',
+        text: 'UPDATE tenaga_kerja SET (jabatan, jmlh, status_hari, id_lap_harian) VALUES (DEFAULT, $1, $2, $3, $4)',
+        values: [
+          jabatanhrini[i], jmlhhrini[i], 'hari ini', rLapHar.rows[0].id,
+        ],
+      };
+      ptenKerjaHrIni.push(pool.query(qTenKerjaHrIni));
+    }
+
+    const ptenKerjaBsk = [];
+    let qTenKerjaBsk;
+    for (let i = 0; i < jabatanbsk.length; i += 1) {
+      qTenKerjaBsk = {
+        text: 'INSERT INTO tenaga_kerja (id, jabatan, jmlh, status_hari, id_lap_harian) VALUES (DEFAULT, $1, $2, $3, $4)',
+        values: [
+          jabatanbsk[i],
+          jmlhbsk[i],
+          'besok',
+          rLapHar.rows[0].id,
+        ],
+      };
+      ptenKerjaBsk.push(pool.query(qTenKerjaBsk));
+    }
+
+    const qkondCuaca = {
+      text: 'INSERT INTO kond_cuaca (id, baik, mendung, hujan_tinggi, hujan_rendah, id_lap_harian) VALUES (DEFAULT, $1, $2, $3, $4, $5)',
+      values: [
+        baikStr,
+        mendungStr,
+        hujanTinggiStr,
+        hujanRendahStr,
+        rLapHar.rows[0].id,
+      ],
+    };
+
+    const pAlatKerja = [];
+    let qAlatKerja;
+    for (let i = 0; i < alat.length; i += 1) {
+      qAlatKerja = {
+        text: 'INSERT INTO alat_kerja (id, alat, qty, id_lap_harian) VALUES (DEFAULT, $1, $2, $3)',
+        values: [
+          alat[i],
+          qty[i],
+          rLapHar.rows[0].id,
+        ],
+      };
+      pAlatKerja.push(pool.query(qAlatKerja));
+    }
+
+    const pNote = [];
+    let qNote;
+    for (let i = 0; i < masalah.length; i += 1) {
+      qNote = {
+        text: 'INSERT INTO note (id, masalah, solusi, id_lap_harian) VALUES (DEFAULT, $1, $2, $3)',
+        values: [
+          masalah[i],
+          solusi[i],
+          rLapHar.rows[0].id,
+        ],
+      };
+      pNote.push(pool.query(qNote));
+    }
+
+    try {
+      await Promise.all(ptenKerjaHrIni);
+      await Promise.all(ptenKerjaBsk);
+      await pool.query(qkondCuaca);
+      await Promise.all(pAlatKerja);
+      await Promise.all(pNote);
+    } catch (e) {
+      throw new InvariantError(e);
+    }
+    return res.status(201).send({
+      status: 'success',
+      message: 'laporan has been created successfully',
+    });
+  } catch (e) {
+    if (e instanceof ClientError) {
+      res.status(e.statusCode).send({
+        status: 'fail',
+        message: e.message,
+      });
+    }
+    return res.status(500).send({
+      status: 'error',
+      message: e.message,
+    });
+  }
+};
+
 module.exports = {
   createLaporan,
   getLaporanByNoProyekKont,
@@ -584,4 +828,5 @@ module.exports = {
   updateBastStatus,
   previewPdf,
   createLapHarian,
+  getDetailLapHarian,
 };
